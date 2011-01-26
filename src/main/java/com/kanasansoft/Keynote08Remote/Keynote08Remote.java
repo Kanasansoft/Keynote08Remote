d3ebfe30d84344af722839c2970ce4ca6a2e47d5
Kanasansoft <kanasansoft@gmail.com>
2011-01-27 00:46:22 +0900

    Keynoteがインストールされていない場合は、ダイアログで通知後にアプリケーションを終了させる処理を追加。

--- src/main/java/com/kanasansoft/Keynote08Remote/Keynote08Remote.java
+++ src/main/java/com/kanasansoft/Keynote08Remote/Keynote08Remote.java
@@ -1,5 +1,12 @@
 package com.kanasansoft.Keynote08Remote;
 
+import java.awt.Button;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Label;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.Robot;
@@ -8,7 +15,11 @@ import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URL;
+
+import javax.script.ScriptException;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -21,12 +32,64 @@ import org.eclipse.jetty.websocket.WebSocket.Outbound;
 public class Keynote08Remote implements OnMessageObserver{
 
 	Robot robot = new Robot();
+	Keynote08Wrapper keynote = null;
 
 	public static void main(String[] args) throws Exception {
 		new Keynote08Remote();
 	}
 
 	public Keynote08Remote() throws Exception {
+
+		try {
+			keynote = new Keynote08Wrapper();
+		} catch (ScriptException e) {
+
+			e.printStackTrace();
+
+			Dialog alert = new Dialog(new Frame(), "Keynote08Remote", true);
+
+			alert.setSize(300, 150);
+			alert.setResizable(false);
+			alert.setLocationByPlatform(true);
+			alert.setAlwaysOnTop(true);
+
+			GridBagLayout layout = new GridBagLayout();
+			alert.setLayout(layout);
+			GridBagConstraints gbc = new GridBagConstraints();
+
+			Label lbl = new Label(e.getMessage(),Label.CENTER);
+			gbc.gridx=0;
+			gbc.gridy=0;
+			gbc.insets=new Insets(5, 5, 5, 5);
+			layout.setConstraints(lbl, gbc);
+
+			Button btn = new Button("Quit");
+			gbc.gridx=0;
+			gbc.gridy=1;
+			gbc.insets=new Insets(5, 5, 5, 5);
+			layout.setConstraints(btn, gbc);
+
+			alert.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					((Dialog)e.getSource()).setVisible(false);
+				}
+			});
+			btn.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg) {
+					((Button)arg.getSource()).getParent().setVisible(false);
+				}}
+			);
+
+			alert.add(lbl);
+			alert.add(btn);
+
+			alert.setVisible(true);
+
+			System.exit(0);
+
+		}
+
 		MenuItem quitMenuItem = new MenuItem("Quit");
 		quitMenuItem.addActionListener(new ActionListener() {
 			@Override
